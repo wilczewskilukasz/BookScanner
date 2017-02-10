3f601dcff9f0a9d91285961bfedba3b9374fd80f
wilczewskilukasz@gmail.com <git@30.10.89>
2017-02-10 19:18:51 +0100

    permission

--- app/src/main/java/com/soldiersofmobile/bookscanner/api/BookApi.java
+++ app/src/main/java/com/soldiersofmobile/bookscanner/api/BookApi.java
@@ -3,9 +3,12 @@ package com.soldiersofmobile.bookscanner.api;
 import com.soldiersofmobile.bookscanner.api.model.BooksResponse;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 
 public interface BookApi {
 
     //TODO add missing Retrofit annotations
+    @GET("/isbn")
+
     Call<BooksResponse> getBook(String isbn);
 }
