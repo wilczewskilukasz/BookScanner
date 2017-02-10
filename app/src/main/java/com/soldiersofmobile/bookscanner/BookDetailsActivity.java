bad2205ee7b9a1bf3a4b28aca6ed19baf55d8120
wilczewskilukasz@gmail.com <git@30.10.89>
2017-02-10 19:21:07 +0100

    activity 1

---
+++ app/src/main/java/com/soldiersofmobile/bookscanner/BookDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.soldiersofmobile.bookscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
